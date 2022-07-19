99a82115ff722796fc1108ab464f507dcb1c0c23
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-07-19 13:29:27 +0200

    added list with removable elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+var list = document.querySelector("ul");
+
+list.addEventListener("click", function (e) {
+    if (e.target.className == "remove") {
+        const element = e.target.parentElement;
+        list.removeChild(element);
+    }
+})
